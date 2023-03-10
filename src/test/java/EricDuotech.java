902169795d3fd647a8d8091d94ae5632b5cf1337
Eric Enkhbold <e.bayarsaikhan@outlook.com>
2023-03-09 22:34:17 -0500

    Duomusic test

--- src/test/java/EricDuotech.java
+++ src/test/java/EricDuotech.java
@@ -1,82 +1,68 @@
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
 public class EricDuotech {
 
 
-
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new EdgeDriver();
         Faker faker = new Faker();
 
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait( Duration.ofSeconds( 5 ) );
 
-        driver.get("http://duotify.us-east-2.elasticbeanstalk.com/register.php");
 
-        // Step 2: Verify the page title
-        String expectedTitle = "Welcome to Duotify!";
-        String actualTitle = driver.getTitle();
-        Assert.assertEquals(actualTitle, expectedTitle);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        // Step 3: Click on Signup here
-        WebElement signupLink = driver.findElement(By.linkText("Signup here"));
-        signupLink.click();
+            driver.get( "http://duotify.us-east-2.elasticbeanstalk.com/register.php" );
 
-        // Step 4: Fill out the form with the required info using Faker class
+            // driver.findElement( By.id( "Welcome to Duotify!" ) );
+            driver.findElement( By.id( "hideLogin" ) ).click();
+        String username = faker.name().username();
         String firstName = faker.name().firstName();
         String lastName = faker.name().lastName();
         String email = faker.internet().emailAddress();
-        String username = faker.name().username();
+        String email2 = faker.internet().emailAddress();
         String password = faker.internet().password();
-        String day = Integer.toString(faker.number().numberBetween(1, 28));
-        String month = Integer.toString(faker.number().numberBetween(1, 12));
-        String year = Integer.toString(faker.number().numberBetween(1900, 2022));
+        String password2 = faker.internet().password();
 
-        WebElement firstNameField = driver.findElement(By.name("firstname"));
-        firstNameField.sendKeys(firstName);
 
-        WebElement lastNameField = driver.findElement(By.name("lastname"));
-        lastNameField.sendKeys(lastName);
+            driver.findElement( By.id( "username" ) ).sendKeys( firstName + lastName );
 
-        WebElement emailField = driver.findElement(By.name("email"));
-        emailField.sendKeys(email);
+            driver.findElement( By.id( "firstname" ) ).sendKeys( firstName );
 
-        WebElement usernameField = driver.findElement(By.name("username"));
-        usernameField.sendKeys(username);
+            driver.findElement( By.id( "lastname" ) ).sendKeys( lastName );
 
-        WebElement passwordField = driver.findElement(By.name("password"));
-        passwordField.sendKeys(password);
+            driver.findElement( By.id( "email" ) ).sendKeys( email );
+            driver.findElement( By.id( "email2" ) ).sendKeys( email );
 
-        WebElement dayField = driver.findElement(By.name("day"));
-        dayField.sendKeys(day);
+            driver.findElement( By.id( "password" ) ).sendKeys( password );
+            driver.findElement( By.id( "password2" ) ).sendKeys( password );
 
-        WebElement monthField = driver.findElement(By.name("month"));
-        monthField.sendKeys(month);
+            driver.findElement( By.name( "registerButton" ) ).click();
+       // driver.findElement(By.id("logout")).click();
+      //  driver.quit();
 
-        WebElement yearField = driver.findElement(By.name("year"));
-        yearField.sendKeys(year);
 
-        // Step 5: Click on Sign up
-        WebElement signUpButton = driver.findElement(By.name("submit"));
-        signUpButton.click();
+      //  WebElement homePageText = driver.findElement(By.className("pageHeadingBig"));
+        //String expectedHomePageText = "You Might Also Like";
+       // String actualHomePageText = homePageText.getText();
+        //Assert.assertEquals(actualHomePageText, expectedHomePageText);
 
-        // Step 6: Verify the URL after logging in
-        String expectedUrl = "http://duotify.us-east-2.elasticbeanstalk.com/browse.php?";
-        String actualUrl = driver.getCurrentUrl();
-        Assert.assertEquals(actualUrl, expectedUrl);
 
-        // Step 7: Verify the name on the left navigation bar
-        WebElement nameLink = driver.findElement(By.cssSelector("a[href='user_details.php']"));
-        String expectedName = firstName + " " + lastName;
-        String actualName = nameLink.getText();
-        Assert.assertEquals(actualName, expectedName);
+        driver.get( "http://duotify.us-east-2.elasticbeanstalk.com/register.php" );
 
-        // Step 8: Verify the name on the main window
-        nameLink.click();
-        WebElement nameElement = driver.findElement(By.cssSelector("div#name>h1"));
+       driver.findElement( By.id( "loginUsername" ) ).sendKeys( firstName + lastName );
+
+       driver.findElement(By.id("loginPassword")).sendKeys(password);
+       driver.findElement(By.name("loginButton")).click();
+
+
+
+
+       driver.quit();
+
 
     }
 
